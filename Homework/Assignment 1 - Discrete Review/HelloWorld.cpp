eb3552f65d3b9feebd78f29a1520cb2da496945a
thesonofwil <thesonofwil@gmail.com>
2022-01-31 10:01:44 -0600

    wrote program

---
+++ Homework/Assignment 1 - Discrete Review/HelloWorld.cpp
@@ -0,0 +1,48 @@
+/**
+ * Wilson Tjoeng (tjoeng@wisc.edu)
+ * 2/2/21
+ * CS577 - Introduction to Algorithms
+ * Assignment 1
+ * 
+ * @brief Simple hello program that takes in inputs, and prints hello to each line up to n times
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+using std::vector;
+
+/**
+ * @brief Driver program
+ * 
+ * @return int always returns 1
+ */
+int main() {
+    string s; 
+    int n;
+    vector<string> strings;
+
+    getline(cin, s); // Get the integer as a string
+    n = stoi(s); // Convert string to integer
+
+    // Store string inputs
+    while (n > 0) {
+        getline(cin, s);
+        strings.push_back(s);
+        n--;
+    }
+
+    // Print strings
+    for (string i : strings) {
+        cout << "Hello, " + i + "!" << endl;
+    }
+
+    return 1;
+}
