2b5d007d827630ff3b95fe3b74eedbf11ebbe70d
thesonofwil <thesonofwil@gmail.com>
2022-02-06 14:54:28 -0600

    updated comments

--- Homework/Assignment 2 - Asymptotic Analysis & Graphs/src/DFS.java
+++ Homework/Assignment 2 - Asymptotic Analysis & Graphs/src/DFS.java
@@ -1,5 +1,4 @@
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.SortedSet;
@@ -13,48 +12,27 @@ import java.util.TreeSet;
  * Course:	   CS577.002
  * Due:		   02/9/22
  *
- * Undirected graph implementation. Note part of this code is from my own Graph implementation
- * from CS400.
+ * Depth-first search implementation. This program takes in user input to create the graph(s) and
+ * then prints out the DFS traversal of it. Note part of this code is from my own Graph 
+ * implementation from CS400.
  */
 public class DFS {
 
-    private static Graph[] graphs;
-
-    
-    // public void construct_graph(String filename) {
-    //     try {
-    //         File input = new File(filename);
-    //         Scanner fileReader = new Scanner(input); // New scanner to read input
-
-    //         // Parse file and add edges
-    //         int numInstances = fileReader.nextInt();
-    //         for (int instance = 0; instance < numInstances; instance++) {
-    //             int numNodes = fileReader.nextInt();
-    //             for (int node = 0; node < numNodes; node++) {
-    //                 String line = fileReader.nextLine();
-    //                 String[] vertices = line.split(" ");
-    //                 if (vertices.length > 1) {
-    //                     for (String v : vertices) {
-    //                         graph.addEdge(vertices[0], v);
-    //                 }
-    //             }
-    //         }
-    //     } catch (FileNotFoundException e) {
-    //         System.err.println("Error: file not found");
-    //         return;
-    //     }
-    // }
-    // }
+    private static Graph[] graphs; // array of graphs 
 
     private static void parse_input() {
         Scanner input = new Scanner(System.in);
 
         int numInstances = input.nextInt(); // Get number of components
         graphs = new Graph[numInstances]; // Allocate memory to create subgraphs
+
+        // Keep looping according to the number of components
         for (int numInstance = 0; numInstance < numInstances; numInstance++) {
             graphs[numInstance] = new Graph(); // Initialize graph
             int numNodes = input.nextInt(); // Get number of nodes per component
             input.nextLine(); // Read the rest of the line to go to the next line
+            
+            // Each line represents a vertex and its edges
             while (numNodes > 0) {
                 String line = input.nextLine();
                 String[] vertices = line.split(" "); // Split line by space
